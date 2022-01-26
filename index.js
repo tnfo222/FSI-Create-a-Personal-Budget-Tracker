396c2dccf00f190eb985f5bd152f870c536b3875
tnfo222 <github.com/tnfo222/psychic-disco>
2022-01-25 19:51:33 -0500

    added weekly, monthly, and annual expense question prompts for calculation

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-// Your Code Here
+for(i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[0]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(i = 0; i < annualExpenseQuestions.length; i++){
+        let answer = parseFloat(window.prompt(annualExpenseQuestions[0]))
+        annualExpenses = annualExpenses + answer
+}
